a025b08fdb2054f36f6aae1430ee768b1e7b5510
Nicole Slater <nicoleslater@pursuit.org>
2023-08-13 15:04:15 -0400

    Updating

--- src/Components/PetList.jsx
+++ src/Components/PetList.jsx
@@ -1,54 +1,24 @@
-import React from "react";
-
-
+import { useState } from "react";
+import React, { useEffect } from "react";
 
 export const PetList = ({pets, matchPets, employee}) => {
 
-
-
-
-  function getPets(employee, pets) {
-    const matchedPets = pets.filter((pet) => pet.employeeId === employee.id)
-     if (matchedPets.length > 0) {
-       return matchPets(employee, pets) 
-     } else {
-       return (
-        <aside className="pets-list">
-       <p> No pets listed for this employee.</p>
-       </aside>
-
-     )}
-   }
+function getPets(employee, pets) {
+ const matchedPets = pets.filter((pet) => pet.employeeId === employee.id)
+  if (matchedPets.length > 0) {
+    return matchPets(employee, pets) 
+  } else {
+    return <p>No pets listed for this employee.</p>
   }
+}
 
-  
+    
+  return (
+    <aside className="pets-list">
 
-  
-  
- 
-// function getPetList(){
-//     fetch("https://one0-2-vet-api.onrender.com/api/pets")
-//     .then ((data) => (data.json()))
-//     .then((json) => {
-//       setPets(json)
-//     })
-//     .catch((error) => {
-      // console.log(error);
-  //   });
-  // }
-
-  // useEffect(() => {
-  //   getPetList()
-  // }, [])
-  // console.log(pet)
-
-  // console.log(pet.employeeId)
-
-//   return (
-//     <aside className="pets-list">
-//       <p>No pets listed for this employee.</p>
-//     </aside>
-//   );
-// };
+      {getPets(employee, pets)}
+    </aside>
+  );
+};
 
 export default PetList;
