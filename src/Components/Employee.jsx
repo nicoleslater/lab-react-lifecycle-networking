18acbba57599850e7ed8db263e09cc97770ed7bf
Nicole Slater <nicoleslater@pursuit.org>
2023-08-13 15:02:55 -0400

    Updating

--- src/Components/Employee.jsx
+++ src/Components/Employee.jsx
@@ -1,12 +1,25 @@
 import PetList from "./PetList";
 import "./Employee.css";
+import { useEffect, useState } from "react";
 
-export const Employee = ({employee, pets}) => {
 
-  function matchPets(employee, pets) {
+export const Employee = ({employee, pets, getPetList}) => {
+  
+  const [showPets, setShowPets] = useState(false)
+
+  function getFullName(employee) {
+    if (employee.prefix) {
+      return `${employee.prefix} ${employee.firstName} ${employee.lastName}`
+    } else if (employee.postfix) {
+      return `${employee.firstName} ${employee.lastName}, ${employee.postfix}`
+    } else {
+      return `${employee.firstName} ${employee.lastName}`
+    }
+  }
+
+
+  function matchPets (employee, pets) {
     const matchedPets = pets.filter((pet) => pet.employeeId === employee.id)
-
-    // console.log(matchedPets)
 
     return (
       <div>
@@ -17,31 +30,21 @@ export const Employee = ({employee, pets}) => {
     )
   }
 
-function handleClick(){
-      setShowPets(!showPets)
-}
 
-function getFullName (employee){
-if (employee.prefix){
-  return `${employee.prefix} ${employee.firstName} ${employee.lastName}`
-}
-
-if (employee.postfix){
-  return `${employee.firstName} ${employee.lastName}, ${employee.postfix} `
-} else {
-  return `${employee.firstName} ${employee.lastName}`
-}
-}
-  // console.log(employee.firstName)
-  // console.log(employee.prefix)
+  function handleClick (){
+    setShowPets(!showPets)
+  }
 
   return (
     <article className="employee">
       <h3>{getFullName(employee)}</h3>
       <h4>{employee.title}</h4>
-    <button  onClick={handleClick}>Show Pets</button>
-      <PetList matchPets={matchPets} pets={pets} 
-      employee={employee}/>
+      <button onClick={handleClick}>Show Pets</button>
+      
+      {showPets && (
+        <PetList matchPets={matchPets} pets={pets} employee={employee} />
+      )}
+      
     </article>
   );
 };
